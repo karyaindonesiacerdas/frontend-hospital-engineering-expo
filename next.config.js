435f7cb75e95e0754924fecb74ceb4c7eb43b96f
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-09-23 12:11:26 +0700

    add security headers

--- next.config.js
+++ next.config.js
@@ -8,4 +8,25 @@ module.exports = {
     domains: ["via.placeholder.com", "images.unsplash.com", "ui-avatars.com"],
   },
   i18n,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
 };
