cba2fb7fcd204d990d30fa050b9a9def975d1459
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-09-29 10:35:15 +0700

    hide registration

--- pages/login.tsx
+++ pages/login.tsx
@@ -154,7 +154,7 @@ const Login: NextPage = () => {
           </div>
         </form>
 
-        <div className="mt-6">
+        {/* <div className="mt-6">
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300"></div>
@@ -181,7 +181,7 @@ const Login: NextPage = () => {
               </Link>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </AuthPageLayout>
   );
