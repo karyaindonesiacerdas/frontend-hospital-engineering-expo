986543f05234fb2c546b877ab1e85aff17b9365c
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-09-29 15:19:43 +0700

    .

--- components/main-hall/Advertisement2.tsx
+++ components/main-hall/Advertisement2.tsx
@@ -13,7 +13,7 @@ export const Advertisement2 = ({ url }: { url?: string }) => {
         <iframe
           width="100%"
           height="100%"
-          src={`https://www.youtube.com/embed/${youtubeId}?version=3&autoplay=1&mute=1&loop=1`}
+          src={`https://www.youtube.com/embed/${youtubeId}?version=3&autoplay=1&playlist=${youtubeId}&mute=1&loop=1`}
           title="YouTube video player"
           frameBorder="0"
           allow="autoplay; loop; fullscreen"
