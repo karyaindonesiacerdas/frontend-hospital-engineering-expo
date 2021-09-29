6b9a1c26dd35a404205fa0140bfbea6716e8a167
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-09-29 11:22:20 +0700

    default adv video

--- components/main-hall/Advertisement2.tsx
+++ components/main-hall/Advertisement2.tsx
@@ -3,7 +3,10 @@ import { youtubeParser } from "utils";
 import styles from "./Advertisement2.module.css";
 
 export const Advertisement2 = ({ url }: { url?: string }) => {
-  const youtubeId = youtubeParser(url || "") || "CXg2xeULoa0";
+  const defaultVideo =
+    "https://www.youtube.com/watch?v=jS0qVrpKjY4&ab_channel=HospitalEngineeringExpo";
+
+  const youtubeId = youtubeParser(url || defaultVideo) || "jS0qVrpKjY4";
 
   return (
     <div className={styles.advertisement}>
