7a8e272f1083791b154b1065b270fb3b2ae1aee5
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-10-11 16:38:48 +0700

    add watch on youtube live

--- components/seminar-room/SeminarLink.tsx
+++ components/seminar-room/SeminarLink.tsx
@@ -6,15 +6,26 @@ import { useSettings } from "hooks/useSettings";
 
 export const SeminarLink = () => {
   const { data } = useSettings();
-  // console.log({ data });
+  console.log({ data });
   return (
     <div className={styles.link}>
       <div className="grid grid-cols-2 gap-0.5 sm:gap-2 p-0 sm:p-1 rounded-lg transform -translate-y-3 sm:translate-y-0 bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg">
-        <Link href="/webinar-schedule">
+        {/* <Link href="/webinar-schedule">
           <a className="flex items-center justify-center py-1 sm:py-0 bg-primary-50 text-primary-500 hover:text-white font-bold sm:rounded-md cursor-pointer hover:bg-primary-500 transition">
             <span className="text-[1vw]">Webinar Schedule</span>
           </a>
-        </Link>
+        </Link> */}
+        <a
+          className="flex items-center justify-center py-1 sm:py-0 bg-red-50 text-red-500 hover:text-white font-bold sm:rounded-md cursor-pointer hover:bg-red-500 transition"
+          href={data?.webinar_link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/youtube.png" alt="zoom logo" />
+          <span className="hidden lg:inline-block text-[1vw]">
+            Watch on Youtube
+          </span>
+        </a>
         <a
           className="flex items-center justify-center py-1 sm:py-0 bg-blue-50 text-blue-500 hover:text-white font-bold sm:rounded-md cursor-pointer hover:bg-blue-500 transition"
           href={data?.zoom_link}
